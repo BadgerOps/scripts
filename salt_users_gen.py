27984f01fc9e17d477ffdc3c8eda3c5c2eb533a4
BadgerOps <github@badgerops.net>
2017-08-30 16:48:40 -0600

    restructure and comment things

--- salt_users_gen.py
+++ salt_users_gen.py
@@ -5,7 +5,7 @@ import pwd
 import yaml
 import fnmatch
 
-root_dir = '/home'
+user_dir = '/home'
 output_dir = '/tmp'
 
 class YamlUsergen(object):
@@ -18,61 +18,81 @@ class YamlUsergen(object):
     """
 
     def __init__(self):
+        """
+        Set up initial items, right now just a list and couple dictionaries
+        """
         self.users = []
         self.userkeys = {}
         self.combined = {}
 
     def get_users(self):
-        home_dir = '/home'
-        print("getting list of users")
-        users = os.listdir(home_dir)
-        print("There are {} users in /home, they are: \n".format(len(users)))
-        for user in users:
-            print(user)
-        res = raw_input("Do these all look like valid users?  Y/N \n") # if running in python 3.6 change this to input() not raw_input()
-        if 'y' in res.lower():
-            self.users = users
-        else:
-            print("ok, bailing out - remove invalid users from /home and try again \n")
-            exit(1)
+        """
+        Generate a list of users based on who's got a /home folder. You can change this or modify
+        this if you've got users that either have separate home folders or no home folders
+        """
 
-    def get_keys(self):
+        print("getting list of users")
+        users = os.listdir(user_dir)
+        print("found {0} users".format(len(users)))
+        return users
+
+    def get_keys(self, users):
+        """
+        Create a dictionary of usernames, and their ssh key(s)
+        """
         print("getting users keys")
-        for user in self.users:
+        for user in users:
             keys = []
             try:
-                for fn in os.listdir('{}/{}/.ssh'.format(root_dir, user)):
-                    if fnmatch.fnmatch(fn, 'authorized_keys*'):
-                        with open('{}/{}/.ssh/{}'.format(root_dir, user, fn), 'r') as fh:
+                for fn in os.listdir('{0}/{1}/.ssh'.format(user_dir, user)): # sometimes authorized_keys is authorized_keys2 /shrug
+                    if fnmatch.fnmatch(fn, 'authorized_keys*'): # use fnmatch to fuzzy match authorized_keys*
+                        with open('{0}/{1}/.ssh/{2}'.format(user_dir, user, fn), 'r') as fh: # use that matched file (fn) to open and extract keys
                             for line in fh.readlines():
                                 keys.append(line)
                             self.userkeys[user] = keys
             except Exception as e:
-                print("Couldn't find keys for {}/{}/.ssh/authorized_keys, error: {}".format(root_dir, user, e))
+                print("Couldn't find keys for {0}/{1}/.ssh/authorized_keys, error: {2}".format(root_dir, user, e))
 
-    def get_uids(self):
-        """Grab UID's and Full names from /etc/passwd"""
-        print("getting UID's and Full names for {} users".format(len(self.users)))
-        for user in self.users:
+    def get_uids(self, users):
+        """Grab UID's and Full names from /etc/passwd and create a dictionary
+        where the username is the key, and the uid, fullname and ssh_auth_file(s)
+        are a dictionary within the key"""
+        print("getting UID's and Full names for {0} users".format(len(users)))
+        for user in users:
             try:
-                pw = pwd.getpwnam(user)
-                self.combined[user] = {'uid': pw.pw_uid, 'fullname': pw.pw_gecos, 'ssh_auth_file': self.userkeys[user] }
+                pw = pwd.getpwnam(user) # use the pwd built-in to get the /etc/passwd file info (username, gid, fullname)
+                combined[user] = { # key is the username
+                'uid': pw.pw_uid,
+                'fullname': pw.pw_gecos.split(',')[0], # pw_gecos gives us fullname but also some further info. This breaks yaml, so we split on , and select the 0'th value
+                'ssh_auth_file': self.userkeys[user]
+                }
             except Exception as e:
-                print('couldn\'t set UID/fullname for {} error: {}'.format(user, e))
+                print("couldn't set UID/fullname for {0} error: {1}".format(user, e))
                 pass
-        print("Done! Set {} users/uids/keys".format(len(self.combined)))
+        print("Done! Set {0} users/uids/keys".format(len(combined)))
+        return combined
 
-    def write_file(self):
+    def write_file(self, combined):
+        """
+        Now that we've generated our dictionary of users/users info, lets write a yaml
+        file with all that info, which should plug straight into salt, or ansible user generation.
+        """
         with open(output_dir + '/users.yml','w') as fw:
-            yaml.dump(self.combined, fw, explicit_start=True, default_flow_style=False)
-
+            yaml.dump(combined, fw, explicit_start=True, default_flow_style=False)
 
     def run(self):
-        self.get_users()
-        self.get_keys()
-        self.get_uids()
-        self.write_file()
+        """
+        Run each function in sequence, theoretically we could use these independant of each other,
+        even though we probably never will.
+        """
+        self.users = self.get_users()
+        self.get_keys(self.users)
+        self.combined = self.get_uids(self.users)
+        self.write_file(self.combined)
 
 if __name__ == '__main__':
+    """
+    If we're running this interactively, just run()
+    """
     YU = YamlUsergen()
     YU.run()
