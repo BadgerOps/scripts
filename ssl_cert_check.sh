9d6ac92636776271e30bc68ad47a8d79f8a93af5
Sol Roberts <github@badgerops.net>
2023-10-31 08:55:51 +0100

    Update ssl_cert_check.sh
    a few tweaks to handle URL's with no ssl certificates

--- ssl_cert_check.sh
+++ ssl_cert_check.sh
@@ -1,48 +1,41 @@
 #!/bin/bash
-#
-# This script helps to quickly identify SSL certificate age
-# You can set a warning threshold (defaults to 90) to alert for certs about to expire
-# use a newline separated file with URL's to  check
-#
 
+# Initialize variables
 input_file=""
 output_file=""
 warning_days_threshold=90
+timeout_seconds=5
 
+# Function to check SSL Expiry Date
 check_ssl_expiry() {
     local url=$1
     local current_epoch=$(date +%s)
     local ssl_output
     local return_status
 
-    ssl_output=$(echo | openssl s_client -servername "$url" -connect "$url:443" 2>&1)
+    ssl_output=$(echo | timeout $timeout_seconds openssl s_client -servername "$url" -connect "$url:443" 2>&1)
     return_status=$?
 
-    if [ $return_status -ne 0 ]; then
-        echo "Error retrieving SSL certificate for $url. Details: $ssl_output"
-        return
-    fi
+    # Check for successful SSL handshake
+    if echo "$ssl_output" | grep -q 'Verify return code: 0 (ok)'; then
+        local expiry_date=$(echo "$ssl_output" | openssl x509 -noout -enddate | cut -d= -f2)
+        local expiry_epoch=$(date --date="$expiry_date" +%s)
+        local expiry_days_left=$(( ($expiry_epoch - $current_epoch) / 86400 ))
 
-    local expiry_date=$(echo "$ssl_output" | openssl x509 -noout -enddate | cut -d= -f2)
-    local expiry_epoch=$(date --date="$expiry_date" +%s)
-    local expiry_days_left=$(( ($expiry_epoch - $current_epoch) / 86400 ))
-
-    if [ -z "$expiry_date" ]; then
-        echo "No SSL certificate found for $url"
-        return
-    fi
-
-    if [ $expiry_days_left -gt $warning_days_threshold ]; then
-        echo "$url is ok, $expiry_days_left days left"
+        if [ $expiry_days_left -gt $warning_days_threshold ]; then
+            echo "$url is ok, $expiry_days_left days left"
+        else
+            echo "$url is close to expiry, $expiry_days_left days left"
+        fi
     else
-        echo "$url is close to expiry, $expiry_days_left days left"
+        # Report the SSL error, but don't abort the entire script
+        echo "Failed to find SSL certificate for $url. SSL check skipped."
     fi
 }
 
 # Remove 'http://' and 'https://' from a URL
 remove_http_s() {
     local url=$1
-    # Remove both https:// and http:// prefixes
     url="${url/https:\/\//}"
     url="${url/http:\/\//}"
     echo "$url"
@@ -65,7 +58,7 @@ fi
 
 # Check URLs from the input file
 while IFS= read -r url; do
-    # Remove leading and trailing whitespaces and 'https://'
+    # Remove leading and trailing whitespaces, 'http://', and 'https://'
     url=$(echo $url | xargs)
     url=$(remove_http_s "$url")
 
