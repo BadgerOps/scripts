38b0faf69b219b287eb8bf7e2499b18b6c78e892
Sol Roberts <github@badgerops.net>
2024-03-23 21:06:24 +0100

    Update imageCSPmerge.py
    final indentation fix what even is web editor

--- imageCSPmerge.py
+++ imageCSPmerge.py
@@ -9,22 +9,23 @@
 import sys
 import yaml
 from copy import deepcopy
-	mirrors = {}
-	merged_manifest = None
-	while len(sys.argv) > 1:
-		arg = sys.argv.pop()
-		if arg == __file__:
-			break
-		with open(arg) as f:
-			print('using: ' + arg, file=sys.stderr)
-			for manifest in yaml.safe_load_all(f):
-				print('- manifest: ' + manifest['metadata']['name'], file=sys.stderr)
-				if merged_manifest is None:
-					merged_manifest = deepcopy(manifest)
-				manifest_mirrors = manifest.get('spec', {})
-				manifest_mirrors = manifest_mirrors.get('repositoryDigestMirrors', [])
-				for mirror in manifest_mirrors:
-					mirrors[mirror['source']] = mirror
+
+mirrors = {}
+merged_manifest = None
+while len(sys.argv) > 1:
+    arg = sys.argv.pop()
+    if arg == __file__:
+        break
+    with open(arg) as f:
+        print('using: ' + arg, file=sys.stderr)
+        for manifest in yaml.safe_load_all(f):
+            print('- manifest: ' + manifest['metadata']['name'], file=sys.stderr)
+            if merged_manifest is None:
+                merged_manifest = deepcopy(manifest)
+            manifest_mirrors = manifest.get('spec', {})
+            manifest_mirrors = manifest_mirrors.get('repositoryDigestMirrors', [])
+            for mirror in manifest_mirrors:
+                mirrors[mirror['source']] = mirror
 mirrors = sorted([mirror for _, mirror in mirrors.items()], key=lambda x: x['source'])
 merged_manifest['metadata']['name'] = 'merged-0'
 merged_manifest['spec']['repositoryDigestMirrors'] = mirrors
